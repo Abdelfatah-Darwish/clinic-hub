84ae7465163fda1d7f4ab3c2508d52acda0a3902
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2026-07-23 11:49:56 +0300

    ✨ feat: add a logo

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,12 +1,21 @@
+import 'package:doctor_appointment_v2/features/onboarding/widgets/doc_logo_and_name.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('Onboarding Screen', textAlign: TextAlign.center),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+            child: Column(children: [DocLogoAndName()]),
+          ),
+        ),
+      ),
     );
   }
 }
