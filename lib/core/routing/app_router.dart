6ef7ead85b191d23f540c9991e6d74811544b5b9
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2026-07-23 07:13:52 +0300

    ✨ feat: create onboarding flow

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,28 @@
+import 'package:doctor_appointment_v2/core/routing/routes.dart';
+import 'package:doctor_appointment_v2/features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    //final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      // case Routes.loginScreen:
+      //   return MaterialPageRoute(builder: const LoginScreen());
+
+      // case Routes.signUpScreen:
+      //   return MaterialPageRoute(builder: const SignupScreen());
+      // case Routes.homeScreen:
+      //   return MaterialPageRoute(builder: const HomeScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => Scaffold(
+          body: Center(
+            child: Text('No route defined for ${settings.name}'),
+          ),
+        ),);
+    }
+  }
+}
---
+++ lib/features/onboarding/onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Onboarding Screen', textAlign: TextAlign.center),
+    );
+  }
+}
