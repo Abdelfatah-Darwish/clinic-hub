dbfaa817c19b172fd34223bbf8e5daed9d2e0a83
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2026-08-09 16:12:33 +0300

    ✨ feat: add SizedBox extentions for help. ✨ feat: add welcom back text

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:doctor_appointment_v2/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,8 +8,22 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Center(child: Text('Login Screen')),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(' Welcome Back', style: TextStyles.font24BlueBold),
+                Text(
+                  ' We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font14GrayRegular,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
